08be3e85ebfd14213433a3d5beafef46852bf65c
kristian welsh <kriswelsh@gmail.com>
2024-08-31 17:17:42 +0100

    asset card second draft

--- src/app/components/asset.tsx
+++ src/app/components/asset.tsx
@@ -1,9 +1,19 @@
-export default function Asset() {
+type AssetProps = {
+    title: string;
+    image: string;
+    description: string;
+}
+
+const asset: React.FC<AssetProps> = ({asset}) => {
+    const {title, description, image} = asset
     return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <h3>header text</h3>
-      <img src="./nyan.png"></img>
-      <p>nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan</p>
-    </div>
+        <div className="w-96 rounded-xl overflow-hidden shadow-lg p-8 bg-slate-500/20">
+          <h3>{title}</h3>
+          <div className="py-5 flex justify-center items-center"><img src={image}></img></div>
+          <p>{description}</p>
+        </div>
     );
 }
+
+export default asset
+
