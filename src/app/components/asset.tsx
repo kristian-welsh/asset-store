825ed763475fbd5160e51ea00700a5a2c9d82f91
kristian welsh <kriswelsh@gmail.com>
2024-08-31 16:07:55 +0100

    first asset list draft

---
+++ src/app/components/asset.tsx
@@ -0,0 +1,9 @@
+export default function Asset() {
+    return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <h3>header text</h3>
+      <img src="./nyan.png"></img>
+      <p>nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan</p>
+    </div>
+    );
+}
