2d506f64a7d0d831e0b12cd672a730632be8ec6f
kristian welsh <kriswelsh@gmail.com>
2024-09-09 03:19:59 +0100

    added link to store preview page

--- src/app/preview/page.tsx
+++ src/app/preview/page.tsx
@@ -1,22 +1,18 @@
-import Asset from "./components/asset.tsx";
+import Asset from "../components/asset.tsx";
+import Navbar from "../components/Navbar.tsx";
 
 const faceAsset = {
   title: "Chat Face",
   image: "./nyan.png",
-  description: "nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan",
+  description: "you have arrived at the store preview page",
 }
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <h1>top nav bar</h1>
-      </div>
-
+      <Navbar/>
       <div className="flex place-items-center">
-      <a href="preview">
         <Asset asset={faceAsset}/>
-      </a>
       </div>
     </main>
   );
